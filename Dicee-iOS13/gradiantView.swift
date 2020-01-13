5a5e944a1f5b440182867916131d0e4606be5ed5
mostafa ayman ahmed <mostafaayman010030@gmail.com>
2020-01-13 23:16:59 +0200

    first commit

---
+++ Dicee-iOS13/gradiantView.swift
@@ -0,0 +1,21 @@
+//
+//  gradiantView.swift
+//  Dicee-iOS13
+//
+//  Created by Moustafa on 12/3/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class gradiantView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
